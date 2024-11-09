40227e044d287efa0ce1afb1339e0ae6cafc7c66
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-11-10 00:55:08 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <NavbarComponent />
+    <v-content class="ma-4">
+      <router-view> </router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavbarComponent from "./components/NavbarComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavbarComponent,
+  },
+};
+</script>
